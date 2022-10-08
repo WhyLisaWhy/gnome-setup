7a379d4022368e82e0c5c4c335e29c0d4f108ff1
Allen Santiago <85431834+WhyLisaWhy@users.noreply.github.com>
2022-10-08 08:21:46 -0400

    Create nobara.sh

---
+++ nobara.sh
@@ -0,0 +1,21 @@
+# Nobara install
+echo "Nobara install Script. Removing trash..."
+
+# package remove
+sudo dnf -y remove onlyoffice-desktopeditors blender obs-studio kdenlive inkscape cheese rhythmbox totem gnome-photos oeg
+
+echo "Trash taken out. Updating System..."
+
+# system update
+sudo dnf upgrade -y
+flatpak update -y
+
+# dual boot time sync
+
+timedatectl set-local-rtc 1
+
+echo "System update comeplete. Installing apps..."
+
+# package install
+sudo dnf -y install libreoffice ffmpegthumbnailer gthumb yaru-theme cabextract xorg-x11-font-utils
+echo "Install complete."
