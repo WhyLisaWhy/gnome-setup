72fa7393c319a93d51937b92c7036f03402023bf
Allen Santiago <85431834+WhyLisaWhy@users.noreply.github.com>
2022-10-09 08:48:41 -0400

    Update nobara.sh

--- nobara.sh
+++ nobara.sh
@@ -6,12 +6,12 @@ mv /home/$USER/gnome-setup/logo-svg/Nobara-logo.svg /home/$USER/Pictures/Nobara-
 
 
 # Nobara install
-echo "Nobara install Script. Removing trash..."
+echo -e "\033[1mNobara install Script. Removing unwanted applications...\033[0m"
 
 # package remove
 sudo dnf -y remove onlyoffice-desktopeditors blender obs-studio kdenlive inkscape cheese rhythmbox totem gnome-photos oeg
 
-echo "Trash taken out. Updating System..."
+echo -e "\033[1mRemoval complete. Updating System...\033[0m"
 
 # system update
 sudo dnf upgrade -y
@@ -21,17 +21,17 @@ flatpak update -y
 
 timedatectl set-local-rtc 1
 
-echo "System update comeplete. Installing apps..."
+echo -e "\033[1mSystem update comeplete. Installing wanted applications...\033[0m"
 
 # package install
-sudo dnf -y install htop libreoffice ffmpegthumbnailer gthumb yaru-theme cabextract xorg-x11-font-utils deja-dup fish pycharm-community
+sudo dnf -y install libreoffice ffmpegthumbnailer gthumb yaru-theme cabextract xorg-x11-font-utils deja-dup fish pycharm-community
 
-# change shell to fish
+# change default shell to fish
 chsh -s /usr/bin/fish
 
 # noisetorch
 
-echo "Install complete. Installing noisetorch."
+echo -e "\033[1mInstall complete. Installing noisetorch.\033[0m"
 
 curl -# https://github.com/noisetorch/NoiseTorch/releases/download/v0.12.2/NoiseTorch_x64_v0.12.2.tgz
 
@@ -41,11 +41,11 @@ gtk-update-icon-cache
 
 sudo setcap 'CAP_SYS_RESOURCE=+ep' ~/.local/bin/noisetorch
 
-echo "Install complete. Installing flatpaks."
+echo -e "\033[1mInstall complete. Installing flatpaks.\033[0m"
 
 flatpak install flathub com.visualstudio.code -y
 
-echo "Install complete."
+echo -e "\033[1mInstall complete.\033[0m"
 
 
 
