1ff0aaaf11689f87cb6e7819a2c46e689c16b3e3
Allen Santiago <85431834+WhyLisaWhy@users.noreply.github.com>
2022-10-11 07:19:59 -0400

    Add files via upload
    FISH config

---
+++ configs/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+neofetch
+
+alias cp="cp -i"                            # Confirm before overwriting something
+alias df='df -h'                                           # Human-readable sizes
+alias free='free -m'                                       # Show sizes in MB
+alias gitu='git add . && git commit && git push'
+alias rm="rm -i"
+alias ls="exa -lah --icons --group-directories-first"
+
